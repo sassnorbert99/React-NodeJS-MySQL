a3b6622e90706fb1ed1937c9cb17766037517e8d
Sass-Gyarmati Norbert <sassnorbert99@gmail.com>
2021-01-12 22:03:23 +0100

    any validates updated
    I uppdated a few things, like if we would like to listen port 3001, just type in and say Hello World without bugs or invalid get requests

--- server/index.js
+++ server/index.js
@@ -1,6 +1,12 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+})
 
 app.listen(3001, () => {
-    console.log('running on port 3001')
-})
+    console.log('running on port 3001');
+});
