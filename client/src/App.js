155ffab131ca01f1afcfdfe368d6f1d5f6bacc2e
Sass-Gyarmati Norbert <sassnorbert99@gmail.com>
2021-01-13 08:33:15 +0100

    neccessary packages are also installed
    Whanever I created a new database connection and connect via server. I also created a simple insert method to check the database correctly working

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,34 @@
 import './App.css';
+import React, {useState, useEffect} from "react";
+import Axios from 'axios';
 
 function App() {
+
+  const [movieName, setMovieName] = useState('');
+  const [review, setReview] = useState('');
+
+  const submitReview = () => {
+    Axios.post('http://localhost:3001/api/insert', {movieName: movieName, movieReview: review})
+    .then(()=>{
+      alert('successful insert');
+    });
+  };
+
   return (
     <div className="App">
       <h1>CRUD APPLICATION</h1>
 
       <div className="form">
         <label>Movie Name:</label>
-        <input type="text" name="movieName" />
+        <input type="text" name="movieName" onChange={(e)=> {
+          setMovieName(e.target.value);
+        }}/>
         <label>Review:</label>
-        <input type="text" name="review" />
+        <input type="text" name="review" onChange={(e)=> {
+          setReview(e.target.value);
+        }}/>
 
-        <button>Submit</button>
+        <button onClick={submitReview}>Submit</button>
       </div>
       
     </div>
--- server/index.js
+++ server/index.js
@@ -1,11 +1,29 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 const app = express();
+const mysql = require('mysql');
 
+const db = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    password: 'sassnorbert99',
+    database: 'CRUDDataBase',
+});
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
+app.post("/api/insert", (req, res)=> {
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-})
+    const movieName = req.body.movieName;
+    const movieReview = req.body.movieReview;
+
+    const sqlInsert = "INSERT INTO movie_reviews (movieName, movieReview) VALUES (?,?)";
+    db.query(sqlInsert, [movieName, movieReview], (err, result)=> {
+        console.log(err);
+    });
+});
 
 app.listen(3001, () => {
     console.log('running on port 3001');
