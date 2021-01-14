468cd5c1768a2cdb5c159f570d7eb2bddded84a9
Sass-Gyarmati Norbert <sassnorbert99@gmail.com>
2021-01-14 19:58:27 +0100

    updated export method
    not working method :(

--- client/src/App.js
+++ client/src/App.js
@@ -27,11 +27,10 @@ function App() {
 
   const [employeeList, setEmployeeList] = useState([]);
 
-
+  //////////////////////////////////////////////////////
   const data = [
-    { firstname: "jill", lastname: "smith", age: 22 },
-    { firstname: "david", lastname: "warner", age: 23 },
-    { firstname: "nick", lastname: "james", age: 26 }
+    { firstname: setName, age: setAge, country: setCountry, position: setPosition, wage: setWage }
+    
   ];
   const camelCase = (str) =>  {
     return str.substring(0, 1).toUpperCase() + str.substring(1);
@@ -40,11 +39,8 @@ function App() {
     // Get column names
     const columns = Object.keys(data[0]);
 
-    // Remove by key (firstname)
     const filterColsByKey = columns.filter(c => c !== 'firstname');
 
-    // OR use the below line instead of the above if you want to filter by index
-    // columns.shift()
 
     return filterColsByKey // OR return columns
   };
